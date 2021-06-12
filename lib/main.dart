7b5c637d474aa3ececee25e18b3e088bb3b85454
FlutLab.io <>
2021-06-12 14:18:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Navegacion Basica',
-    home: PrimeraRuta(),
+    title: 'Demostración de rutas nombradas',
+    // Inicie la aplicación con la ruta con nombre. En nuestro caso, la aplicación comenzará
+    // en el Widget PrimeraPantalla
+    initialRoute: '/',
+    routes: {
+      // Cuando naveguemos hacia la ruta "/", crearemos el Widget PrimeraPantalla
+      '/': (context) => PrimeraPantalla(),
+      // Cuando naveguemos hacia la ruta "/segunda", crearemos el Widget SegundaPantalla
+      '/segunda': (context) => SegundaPantalla(),
+    },
   ));
 }
 
-class PrimeraRuta extends StatelessWidget {
+class PrimeraPantalla extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Primera Ruta'),
+        title: Text('Primera Pantalla'),
       ),
       body: Center(
         child: RaisedButton(
-          child: Text('Abrir Ruta'),
+          child: Text('Pantalla de Inicio'),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SegundaRuta()),
-            );
+            // Navega a la segunda pantalla usando una ruta con nombre
+            Navigator.pushNamed(context, '/segunda');
           },
         ),
       ),
@@ -29,16 +35,18 @@ class PrimeraRuta extends StatelessWidget {
   }
 }
 
-class SegundaRuta extends StatelessWidget {
+class SegundaPantalla extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Segunda Ruta"),
+        title: Text("segunda Screen"),
       ),
       body: Center(
         child: RaisedButton(
           onPressed: () {
+            // Navega de regreso a la primera pantalla haciendo clic en la ruta actual
+            // fuera de la pila
             Navigator.pop(context);
           },
           child: Text('Regresar!'),
